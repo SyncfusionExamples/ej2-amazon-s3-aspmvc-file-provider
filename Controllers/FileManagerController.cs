c074fed89d048d0f613884184c5ca15ac3b7c2e9
bhuvaneswari <bhuvaneswari.nagarajan@syncfusion.com>
2020-05-18 21:03:36 +0530

    EJ2-39028 - Updated the comments in amazon file provider

--- Controllers/FileManagerController.cs
+++ Controllers/FileManagerController.cs
@@ -1,17 +1,15 @@
-﻿using System.Collections.Generic;
-using System.Web.Mvc;
-using System.Web.Hosting;
-using Newtonsoft.Json;
-using Syncfusion.EJ2.FileManager.AmazonS3MVCFileProvider;
-using Syncfusion.EJ2.FileManager.Base;
+﻿using System;
+using System.Collections.Generic;
 using System.Web;
-using System;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using Syncfusion.EJ2.FileManager.Base;
+using Syncfusion.EJ2.FileManager.AmazonS3MVCFileProvider;
 
 namespace EJ2ASPMVCFileProvider.Controllers
 {
     public class FileManagerController : Controller
     {
-
         public ActionResult FileManager()
         {
             return View();
@@ -25,33 +23,41 @@ namespace EJ2ASPMVCFileProvider.Controllers
 
         public ActionResult FileOperations(FileManagerDirectoryContent args)
         {
-
             switch (args.Action)
             {
                 case "read":
+                    // Reads the file(s) or folder(s) from the given path.
                     return Json(operation.ToCamelCase(operation.GetFiles(args.Path, args.ShowHiddenItems)));
                 case "delete":
+                    // Deletes the selected file(s) or folder(s) from the given path.
                     return Json(operation.ToCamelCase(operation.Delete(args.Path, args.Names)));
                 case "details":
                     if (args.Names == null)
                     {
                         args.Names = new string[] { };
                     }
+                    // Gets the details of the selected file(s) or folder(s).
                     return Json(operation.ToCamelCase(operation.Details(args.Path, args.Names, args.Data)));
                 case "create":
+                    // Creates a new folder in a given path.
                     return Json(operation.ToCamelCase(operation.Create(args.Path, args.Name)));
                 case "search":
+                    // Gets the list of file(s) or folder(s) from a given path based on the searched key string.
                     return Json(operation.ToCamelCase(operation.Search(args.Path, args.SearchString, args.ShowHiddenItems, args.CaseSensitive)));
                 case "copy":
+                    // Copies the selected file(s) or folder(s) from a path and then pastes them into a given target path.
                     return Json(operation.ToCamelCase(operation.Copy(args.Path, args.TargetPath, args.Names, args.RenameFiles, args.TargetData, args.Data)));
                 case "move":
+                    // Cuts the selected file(s) or folder(s) from a path and then pastes them into a given target path.
                     return Json(operation.ToCamelCase(operation.Move(args.Path, args.TargetPath, args.Names, args.RenameFiles, args.TargetData,args.Data)));
                 case "rename":
+                    // Renames a file or folder.
                     return Json(operation.ToCamelCase(operation.Rename(args.Path, args.Name, args.NewName,false,args.Data)));
             }
             return null;
-
         }
+
+        // Uploads the file(s) into a specified path
         public ActionResult Upload(string path, IList<System.Web.HttpPostedFileBase> uploadFiles, string action)
         {
             if (path == null)
@@ -69,22 +75,20 @@ namespace EJ2ASPMVCFileProvider.Controllers
                 Response.StatusDescription = uploadResponse.Error.Message;
                 Response.End();
             }
-
             return Content("");
         }
 
+        // Downloads the selected file(s) and folder(s)
         public ActionResult Download(string downloadInput)
         {
             FileManagerDirectoryContent args = JsonConvert.DeserializeObject<FileManagerDirectoryContent>(downloadInput);
             return operation.Download(args.Path, args.Names);
-
         }
 
-
+        // Gets the image(s) from the given path
         public ActionResult GetImage(FileManagerDirectoryContent args)
         {
             return operation.GetImage(args.Path, args.Id, false, null, null);
         }
-
     }
 }
