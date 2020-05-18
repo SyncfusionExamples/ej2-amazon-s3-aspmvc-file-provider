cbdf18e477cc2de98b1b3bfae32330165a586d1b
bhuvaneswari <bhuvaneswari.nagarajan@syncfusion.com>
2020-05-18 20:36:16 +0530

    feature(EJ2-39028): Publish the amazon file provider service to GitHub

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# Changelog
+
+## [Unreleased]
+
+### Amazon S3 File Provider (ASP.NET MVC)
+
+#### New Features
+
+- Added file provider support for Amazon S3(Simple Storage Service) bucket storage service.
---
+++ Controllers/FileManagerController.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using System.Web.Hosting;
+using Newtonsoft.Json;
+using Syncfusion.EJ2.FileManager.AmazonS3MVCFileProvider;
+using Syncfusion.EJ2.FileManager.Base;
+using System.Web;
+using System;
+
+namespace EJ2ASPMVCFileProvider.Controllers
+{
+    public class FileManagerController : Controller
+    {
+
+        public ActionResult FileManager()
+        {
+            return View();
+        }
+
+        AmazonS3FileProvider operation = new AmazonS3FileProvider();
+        public FileManagerController()
+        {
+            this.operation.RegisterAmazonS3( "bucketName",  "awsAccessKeyId",  "awsSecretAccessKey", "bucketRegion");
+        }
+
+        public ActionResult FileOperations(FileManagerDirectoryContent args)
+        {
+
+            switch (args.Action)
+            {
+                case "read":
+                    return Json(operation.ToCamelCase(operation.GetFiles(args.Path, args.ShowHiddenItems)));
+                case "delete":
+                    return Json(operation.ToCamelCase(operation.Delete(args.Path, args.Names)));
+                case "details":
+                    if (args.Names == null)
+                    {
+                        args.Names = new string[] { };
+                    }
+                    return Json(operation.ToCamelCase(operation.Details(args.Path, args.Names, args.Data)));
+                case "create":
+                    return Json(operation.ToCamelCase(operation.Create(args.Path, args.Name)));
+                case "search":
+                    return Json(operation.ToCamelCase(operation.Search(args.Path, args.SearchString, args.ShowHiddenItems, args.CaseSensitive)));
+                case "copy":
+                    return Json(operation.ToCamelCase(operation.Copy(args.Path, args.TargetPath, args.Names, args.RenameFiles, args.TargetData, args.Data)));
+                case "move":
+                    return Json(operation.ToCamelCase(operation.Move(args.Path, args.TargetPath, args.Names, args.RenameFiles, args.TargetData,args.Data)));
+                case "rename":
+                    return Json(operation.ToCamelCase(operation.Rename(args.Path, args.Name, args.NewName,false,args.Data)));
+            }
+            return null;
+
+        }
+        public ActionResult Upload(string path, IList<System.Web.HttpPostedFileBase> uploadFiles, string action)
+        {
+            if (path == null)
+            {
+                return Content("");
+            }
+            FileManagerResponse uploadResponse;
+            uploadResponse = operation.Upload(path, uploadFiles, action, null);
+            if (uploadResponse.Error != null)
+            {
+                HttpResponse Response = System.Web.HttpContext.Current.Response;
+                Response.Clear();
+                Response.Status = uploadResponse.Error.Code + " " + uploadResponse.Error.Message;
+                Response.StatusCode = Int32.Parse(uploadResponse.Error.Code);
+                Response.StatusDescription = uploadResponse.Error.Message;
+                Response.End();
+            }
+
+            return Content("");
+        }
+
+        public ActionResult Download(string downloadInput)
+        {
+            FileManagerDirectoryContent args = JsonConvert.DeserializeObject<FileManagerDirectoryContent>(downloadInput);
+            return operation.Download(args.Path, args.Names);
+
+        }
+
+
+        public ActionResult GetImage(FileManagerDirectoryContent args)
+        {
+            return operation.GetImage(args.Path, args.Id, false, null, null);
+        }
+
+    }
+}
